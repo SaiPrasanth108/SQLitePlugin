7c781964a5b10dbdba56366d8b6c203ab381d02d
saiprasanthr <er.saiprasanthr@gmail.com>
2021-06-14 10:44:00 +0530

    js method added

--- www/SQLitePlugin.js
+++ www/SQLitePlugin.js
@@ -7,3 +7,29 @@ exports.coolMethod = function (arg0, success, error) {
 exports.createTableWithQuery = function (arg0, success, error) {
   exec(success, error, 'SQLitePlugin', 'createTableWithQuery', [arg0]);
 };
+
+exports.insert = function (
+  query,
+  id,
+  username,
+  userpassword,
+  firstname,
+  lastname,
+  usertype,
+  taskActive,
+  tasks,
+  success,
+  error
+) {
+  exec(success, error, 'DemoPlugin', 'insert', [
+    query,
+    id,
+    username,
+    userpassword,
+    firstname,
+    lastname,
+    usertype,
+    taskActive,
+    tasks,
+  ]);
+};
